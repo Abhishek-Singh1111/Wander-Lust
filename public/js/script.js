93561f9f0fe267c3a7b35039d6f0427a6b947f8f
Abhishek <Abhishek-Singh1111>
2026-05-29 17:44:00 +0530

    Including password hide and unhide button

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,22 @@
     }, false)
   })
 })()
+
+// Password Toggle Functionality
+document.querySelectorAll('.togglePasswordBtn').forEach(btn => {
+  btn.addEventListener('click', function() {
+    const input = this.closest('.input-group').querySelector('input')
+    const icon = this.querySelector('i')
+    
+    if (input.type === 'password') {
+      input.type = 'text'
+      icon.classList.remove('fa-eye')
+      icon.classList.add('fa-eye-slash')
+    } else {
+      input.type = 'password'
+      icon.classList.remove('fa-eye-slash')
+      icon.classList.add('fa-eye')
+    }
+  })
+})
+
